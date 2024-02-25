9505be2956450b596c8a907d90af68d72c8e9590
Jkose7 <ojosema10@gmail.com>
2024-02-25 16:09:17 -0500

    footer maquetacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 //import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
