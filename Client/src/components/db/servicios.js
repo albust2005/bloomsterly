052523702d380c0f668cd03a37bb267190d204d5
Jkose7 <ojosema10@gmail.com>
2024-03-08 15:37:27 -0500

    Optimizacion de imagenes

--- Client/src/components/db/servicios.js
+++ Client/src/components/db/servicios.js
@@ -4,55 +4,55 @@
 export const servicios = [
     {
         uuid: "s111",
-        img: "../src/assets/img_servicios/musica_servicio.jpg",
+        img: "../src/assets/img_servicios/servicio-musica.webp",
         nombre: "Musica",
         descripcion: "Ofrecemos distintos servicios de musica"
     },
     {
         uuid: "s112",
-        img: "../src/assets/img_servicios/decoracion_servicio.jpg",
+        img: "../src/assets/img_servicios/servicio-decoracion.webp",
         nombre: "Decoracion",
         descripcion: "Ofrecemos distintos estilos en el momento de la decoracion de tu evento"
     },
     {
         uuid: "s113",
-        img: "../src/assets/img_servicios/comida_servicio.jpg",
+        img: "../src/assets/img_servicios/servicio-comida.webp",
         nombre: "Alimentacion",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s114",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-cocteleria.webp",
         nombre: "Cocteleria",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s115",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-mobiliario.webp",
         nombre: "Mobiliario",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s116",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-personal.webp",
         nombre: "Personal",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s117",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-entretenimiento.webp",
         nombre: "Entretenimiento",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s118",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-fotografia.webp",
         nombre: "Fotografia",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
     {
         uuid: "s119",
-        img: "",
+        img: "../src/assets/img_servicios/servicio-espacio.webp",
         nombre: "Espacios",
         descripcion: "Ofrecemos distintos de comida para tu evento"
     },
