e84a6c5b4262a4e92302dfd857cb15b4db29e960
NicoNico04 <nicollcanocastro465@gmail.com>
2024-03-18 11:32:34 -0500

    componetizacion login

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,11 +1,9 @@
-//import { Ingresar } from "./inicio_sesion";
-//import { Registrar } from "./registro";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 import "./Login.css";
 import { useState } from "react";
 
-
+import { InicioSesion } from "./inicio_sesion";
+import { Registro } from "./registro";
 
 export function Login() {
 
@@ -22,63 +20,8 @@ export function Login() {
       <div className={`container ${active} w-full dark:bg-second_color_lt`} >
         <div className="curved-shape dark:bg-light_theme"></div>
         <div className="curved-shape2 dark:bg-second_color_lt"></div>
-          <div className="form-box Login left-0 px-[40px]">
-            <h2 className="animation font-semibold">Inicio de Sesión</h2>
-
-            {/*FORM inicio sesion*/}
-
-            <form action="" className="">
-
-              <div className="input-box animation">
-                <input type="text" className="" required />
-                <label htmlFor="Username" className="">
-                  Username
-                </label>
-                <FontAwesomeIcon
-                  icon={faUser}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%] "
-                />
-              </div>
-
-              <div className="input-box animation" >
-                <input type="text" className="" required />
-                <label htmlFor="password" className="">
-                  Password
-                </label>
-                <FontAwesomeIcon
-                  icon={faLock}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%]"
-                />
-              </div>
-
-              <div className="input-box animation " >
-                <button type="submit" className="btn">
-                  Login
-                </button>
-              </div>
-
-              <div className="regi-link animation " >
-                <p>
-                  NO tienes una cuenta?
-                  <a href="#" className="SignUpLink dark:hover:bg-light_theme dark:hover:text-second_color_lt" onClick={ChangeActive}>
-                    Registrate
-                  </a>
-                </p>
-              </div>
-
-              <div className="regi-link animation " >
-                <p>
-                  Eres empresario o emprendedor?
-                  <a href="#" className="SignUpLink  dark:hover:bg-light_theme dark:hover:text-second_color_lt" >
-                    Ingresa aquí
-                  </a>
-                </p>
-              </div>
-
-            </form>
-          </div>
+          
+          <InicioSesion ChangeActive={ChangeActive}></InicioSesion>
 
           <div className="info-content Login " >
             <h2 className="animation">BIENVENIDO DE VUELTA!</h2>
@@ -90,74 +33,7 @@ export function Login() {
 
           {/*FORM registro*/}
 
-          <div className="form-box Register" >
-            <h2 className="animation font-semibold" >Registro</h2>
-
-            <form action="" className="">
-            <div className="input-box animation " >
-                <input type="text" className="" required />
-                <label htmlFor="email" className="">
-                  Correo electronico
-                </label>
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%] "
-                />
-              </div>
-
-              <div className="input-box animation ">
-                <input type="text" className="" required />
-                <label htmlFor="Username" className="">
-                  Nombre de usuario
-                </label>
-                <FontAwesomeIcon
-                  icon={faUser}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%] "
-                />
-              </div>
-
-              <div className="input-box animation " >
-                <input type="text" className="" required />
-                <label htmlFor="password" className="">
-                  Contraseña
-                </label>
-                <FontAwesomeIcon
-                  icon={faLock}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%]"
-                />
-              </div>
-
-              <div className="input-box animation " >
-                <input type="text" className="" required />
-                <label htmlFor="confirm-password" className="">
-                  Confirma tu contraseña
-                </label>
-                <FontAwesomeIcon
-                  icon={faLock}
-                  style={{ color: "FFFFEF" }}
-                  className="absolute top-[50%] right-0 translate-y-[-50%]"
-                />
-              </div>
-
-              <div className="animation ">
-                <button type="submit" className="btn">
-                  Register
-                </button>
-              </div>
-
-              <div className="regi-link animation " >
-                <p>
-                  Dont have an account ?
-                  <a href="#" className="SignInLink  dark:hover:bg-light_theme dark:hover:text-second_color_lt" onClick={ChangeActive}>
-                    Sign In
-                  </a>
-                </p>
-              </div>
-            </form>
-          </div>
+          <Registro ChangeActive={ChangeActive}></Registro>
 
           <div className="info-content Register" >
             <h2 className="animation ">BIENVENIDO DE VUELTA!</h2>
