511c2f8338e1c7f4fc036cef878872bf6a742084
silvana2828 <silvanaxuluaga0@gmail.com>
2024-04-12 01:17:15 -0500

    ¿texto de la sección qué hacemos?

--- Client/src/pages/Index.jsx
+++ Client/src/pages/Index.jsx
@@ -13,67 +13,53 @@ export function LandingPage() {
   return (
     <>
       {/* BloomSterly */}
-      <FirtsTitle 
-        title="BloomSterly"
-      />
+      <FirtsTitle title="BloomSterly" />
       {/*Seccion Nosotros*/}
       <div className="flex flex-col items-center gap-16 xl:gap-32">
-
         <section className="h-full xl:flex gap-5">
-
           <article className="w-full xl:w-3/5 2xl:w-1/2">
+            <Titles title="NOSOTROS" subTitle="¿Qué hacemos?" />
 
-            <Titles
-              title='NOSOTROS'
-              subTitle='¿Qué hacemos?'
-            />
-            
             {/* <Flor name='blanco' positionx={200} positiony={300} size={10}/> */}
 
-            <p className="text-white w-full pt-6 text-1xl sm:text-2xl md:text-3xl lg-text-4xl
-            dark:text-black text-balance">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto
-              vero accusantium rem quas facilis, placeat modi, distinctio
-              exercitationem ipsam necessitatibus vitae enim assumenda,
-              asperiores optio dignissimos. Dolorum recusandae porro reiciendis.
+            <p
+              className="text-white w-full pt-6 text-1xl sm:text-2xl md:text-3xl lg-text-4xl
+            dark:text-black text-balance"
+            >
+              Explora todos los servicios que te ofrecemos, para que puedas
+              diseñar y personalizar tu evento desde la comodidad de tu hogar.
+              Aquí te brindamos todas las herramientas que te permitirán hacer
+              de tu evento una experiencia inolvidable.
             </p>
-
           </article>
 
           <article className="flex xl:w-2/5 2xl:w-1/2 justify-center">
             <img
-              className='aspect-auto w-full object-cover xl:scale-115'
+              className="aspect-auto w-full object-cover xl:scale-115"
               src="../src/assets/img/nosotros_imagen_mesas.webp"
-              alt="" />
+              alt=""
+            />
           </article>
-
         </section>
-
 
         {/*Seccion Conoce*/}
         <section className="flex flex-col w-full gap-5">
-
           <article className="w-full">
-            <Titles
-              title='CONOCE'
-              subTitle='Nuestros servicios'
-            />
+            <Titles title="CONOCE" subTitle="Nuestros servicios" />
           </article>
 
           <article className="mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
-                {categorias.slice(0, 3).map(categoria => (
-                    <div key={categoria.id}>
-                        <PolaroidCard
-                            url={categoria.img}
-                            nombre={categoria.nombre}
-                            descripcion={categoria.descripcion}
-                        />
-                    </div>
-                ))}
-            </article>
-
+            {categorias.slice(0, 3).map((categoria) => (
+              <div key={categoria.id}>
+                <PolaroidCard
+                  url={categoria.img}
+                  nombre={categoria.nombre}
+                  descripcion={categoria.descripcion}
+                />
+              </div>
+            ))}
+          </article>
         </section>
-        
       </div>
     </>
   );
