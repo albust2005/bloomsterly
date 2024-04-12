b75489901e13a133b41c536d4b2280d737dd7a29
Jkose7 <ojosema10@gmail.com>
2024-04-12 01:19:03 -0500

    correcion errores en empresa

--- Client/src/pages/empresa/indexEmpresa.jsx
+++ Client/src/pages/empresa/indexEmpresa.jsx
@@ -1,11 +1,14 @@
 import { ButtonNav } from "../../components/buttons/buttonNav";
+import { useUserContext } from "../../components/providers/userProvider";
 import { TitleAE } from "../../components/titles/titleAE";
 import { LateralMenu } from "./templates/lateralMenu";
 
 export function Index_empresa() {
+    const { sesionUser } = useUserContext()
+
     return (
         <>
-            <TitleAE title="Empresa" descripcion="Bienvenido @Empresa al aplicativo"/>
+            <TitleAE title="Empresa" descripcion={`Bienvenida empresa ${sesionUser.Username}`}/>
         </>
     )
 }
