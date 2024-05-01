4d5e9e3349120549aae24a2474b47bfdf76718b0
silvana2828 <silvanaxuluaga0@gmail.com>
2024-05-01 05:43:44 -0500

    Sección del perfil de admin responsive

--- Client/src/components/buttons/buttonAdmin.jsx
+++ Client/src/components/buttons/buttonAdmin.jsx
@@ -4,7 +4,7 @@ export const ButtonAdmin = ({ children, href, onClick }) => {
     <div>
       <Link to={href} className="text-white w-full rounded-sm  h-full border-2 border-transparent
       bg-hover_boton_admin p-1 font-text hover:bg-transparent hover:border-hover_boton_admin 
-      transition-all ease-in-out
+      transition-all ease-in-out minicel:text-[8px]  md:text-[11px] sm:text-[9px] lg:text-[17px]
       dark:bg-light_theme dark:hover:bg-transparent dark:hover:border-light_theme dark:text-second_color_lt
       dark:hover:text-light_theme" onClick={onClick}>
           {children}
--- Client/src/pages/empresa/perfilEmpresa.jsx
+++ Client/src/pages/empresa/perfilEmpresa.jsx
@@ -46,11 +46,11 @@ export function PerfilEmpresa() {
           >
             <IconUser clasName="h-20" />
             {sesionUser?.Rol === "Administrador" ? (
-              <h2 className="text-white text-4xl dark:text-second_color_lt">
+              <h2 className="text-white dark:text-second_color_lt celular:text-[20px] sm:text-[30px] lg:text-4xl">
                 {dato.nombre}
               </h2>
             ) : (
-              <h2 className="text-white text-4xl dark:text-second_color_lt">
+              <h2 className="text-white text-4xl dark:text-second_color_lt celular:text-[20px] sm:text-[30px] lg:text-4xl">
                 {empresaIngresada?.nombre}
               </h2>
             )}
@@ -58,42 +58,84 @@ export function PerfilEmpresa() {
           <div className="w-[100%] bg-[#190042] rounded-sm p-5 font-title text-white dark:bg-second_color_lt">
             {sesionUser?.Rol === "Administrador" ? (
               <>
-                <p className="font-semibold text-2xl">Cédula</p>
-                <p className="text-lg">{dato.COD}</p>
-                <p className="font-semibold text-2xl">Nombre</p>
-                <p className="text-lg">{dato.nombre}</p>
-                <p className="font-semibold text-2xl">Primer Apellido</p>
-                <p className="text-lg">{dato.primer_apelli}</p>
-                <p className="font-semibold text-2xl">Nombre De Usuario</p>
-                <p className="text-lg">{dato.username}</p>
-                <p className="font-semibold text-2xl">Gmail</p>
-                <p className="text-lg">{dato.email}</p>
-                <p className="font-semibold text-2xl">Municipio</p>
-                <p className="text-lg">{dato.municipio?.municipio}</p>
+                <p className="font-semibold md:text-xl celular:text-[15px] lg:text-2xl">
+                  Cédula
+                </p>
+                <p className="celular:text-[15px] lg:text-[lg]">{dato.COD}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Nombre
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">{dato.nombre}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Primer Apellido
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {dato.primer_apelli}
+                </p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Nombre De Usuario
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {dato.username}
+                </p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Gmail
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">{dato.email}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Municipio
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {dato.municipio?.municipio}
+                </p>
               </>
             ) : (
               <>
-                <p className="font-semibold text-2xl">Descripción</p>
-                <p className="text-lg">{empresaIngresada?.descripcion}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Descripción
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.descripcion}
+                </p>
 
-                <p className=" font-semibold text-2xl">Dirección</p>
-                <p className="text-lg">{empresaIngresada?.direccion}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Dirección
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.direccion}
+                </p>
 
-                <p className="font-semibold text-2xl ">Teléfono</p>
-                <p className="text-lg">{empresaIngresada?.telefono}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Teléfono
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.telefono}
+                </p>
 
-                <p className="font-semibold text-2xl">Correo electrónico</p>
-                <p className="text-lg">{empresaIngresada?.email}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Correo electrónico
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.email}
+                </p>
 
-                <p className="font-semibold text-2xl">Instagram</p>
-                <p className="text-lg">{empresaIngresada?.instagram}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Instagram
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.instagram}
+                </p>
 
-                <p className="font-semibold text-2xl">Facebook</p>
-                <p className="text-lg">{empresaIngresada?.facebook}</p>
+                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                  Facebook
+                </p>
+                <p className="lg:text-lg celular:text-[15px]">
+                  {empresaIngresada?.facebook}
+                </p>
               </>
             )}
 
-            <div className="flex flex-row-reverse gap-4">
+            <div className="flex flex-row-reverse gap-4 mt-2">
               {sesionUser?.Rol === "Administrador" ? (
                 <>
                   <ButtonAdmin onClick={eliminar}>Eliminar</ButtonAdmin>
