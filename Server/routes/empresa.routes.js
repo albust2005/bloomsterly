f308039bee5c53dfbe58d5f50a953ee645101f46
Juanes <juan.henao.garzon@gmail.com>
2024-05-04 02:08:27 -0500

    crear y editar servicios funcionando

--- Server/routes/empresa.routes.js
+++ Server/routes/empresa.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getlogout, editarPerfil, eliminarPerfil, getempresa } from "../controllers/empresa.controller.js";
+import { getlogout, editarPerfil, eliminarPerfil, getempresa, servicio, editarServicio} from "../controllers/empresa.controller.js";
 import jwt from 'jsonwebtoken'
-
+import { upload } from "../multer/multer.js";
 // esto es para poner rutas
 const router = express.Router()
 const verificarToken=(req,res, next)=>{
@@ -26,5 +26,10 @@ router.put("/editarPerfil", verificarToken, editarPerfil);
 // ruta para eliminar el perfil de la empresa
 router.delete("/eliminarPerfil", verificarToken, eliminarPerfil);
 // ruta para obtener la informacion de la empresa por el token
-router.get("/getempresa",verificarToken,getempresa)
+router.get("/getempresa",verificarToken,getempresa);
+// ruta para hacer un servicio la empresa
+router.post("/servicio",verificarToken, upload.array("imagenes") ,servicio);
+// ruta para editar un servicio de la empresa
+router.post("/editarServicio", verificarToken, upload.array("imagenes"), editarServicio);
+
 export default router
