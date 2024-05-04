8d65eee54b67ee2efede10db32d1c847543583b1
Juanes <juan.henao.garzon@gmail.com>
2024-05-04 02:25:09 -0500

    ruta para obtener todos los servicios relacionados a la empresa

--- Server/controllers/empresa.controller.js
+++ Server/controllers/empresa.controller.js
@@ -1,4 +1,4 @@
-import {Municipios,Administradores,Usuarios,Empresas,Reservas,Servicios,Categorias,ControlUsuarios,DescripcionReserva,Fechas,SolicitudEmpresas,AdministradorSolicitud,Subcategorias,ImagesServicios} from '../models/associations.js'
+import { Municipios, Administradores, Usuarios, Empresas, Reservas, Servicios, Categorias, ControlUsuarios, DescripcionReserva, Fechas, SolicitudEmpresas, AdministradorSolicitud, Subcategorias, ImagesServicios } from '../models/associations.js'
 import session from "express-session";
 import bcrypt from 'bcrypt';
 import Sequelize from 'sequelize';
@@ -7,94 +7,94 @@ import path from 'path';
 import { baseDir } from '../server.js';
 
 //Esto cierra la sesion de la empresa
-export const getlogout = async(req,res)=>{
-    req.session.destroy((error)=>{
-        if (error){
-            console.log("error al cerrar sesion ",error)
-            res.status(500).json({message: "error al cerrar sesion"})
-        }else{
+export const getlogout = async (req, res) => {
+    req.session.destroy((error) => {
+        if (error) {
+            console.log("error al cerrar sesion ", error)
+            res.status(500).json({ message: "error al cerrar sesion" })
+        } else {
             res.clearCookie('connect.sid');
-            res.status(200).json({message: "sesion cerrada correctamente"})
+            res.status(200).json({ message: "sesion cerrada correctamente" })
         }
     })
 }
 // Esta funcion es para editar el perfil de la empresa
-export const editarPerfil = async(req,res)=>{
+export const editarPerfil = async (req, res) => {
     const {
-       nombre,
-       descripcion,
-       COD_municipios,
-       instagram,
-       facebook,
-       contrasena,
-       direccion,
-       telefono,
-       email
-    }=req.body
+        nombre,
+        descripcion,
+        COD_municipios,
+        instagram,
+        facebook,
+        contrasena,
+        direccion,
+        telefono,
+        email
+    } = req.body
     try {
-        const hashedPassword = await bcrypt.hash(contrasena,5); 
+        const hashedPassword = await bcrypt.hash(contrasena, 5);
         await Empresas.update({
             nombre,
             descripcion,
             COD_municipios,
             instagram,
             facebook,
-            contrasena:hashedPassword,
+            contrasena: hashedPassword,
             direccion,
             telefono,
             email
-        },{where:{NIT:req.userCOD}})
-        res.status(201).json({message:"Datos del perfil actualizados"})
+        }, { where: { NIT: req.userCOD } })
+        res.status(201).json({ message: "Datos del perfil actualizados" })
     } catch (error) {
         if (error instanceof Sequelize.UniqueConstraintError) {
             // Manejar el error de restricción de unicidad
-            res.status(400).json({message: `Los datos ingresados ya existen en el sistema`, error:error.errors})
-          } else if (error instanceof Sequelize.DatabaseError) {
+            res.status(400).json({ message: `Los datos ingresados ya existen en el sistema`, error: error.errors })
+        } else if (error instanceof Sequelize.DatabaseError) {
             // Manejar el error de base de datos
-            res.status(400).json({message: `Error de base datos`, error:error.message})
-          } else {
+            res.status(400).json({ message: `Error de base datos`, error: error.message })
+        } else {
             // Manejar otros tipos de errores
-            res.status(400).json({message:'Hubo un error al editar perfil', error});
-          }
+            res.status(400).json({ message: 'Hubo un error al editar perfil', error });
         }
+    }
 }
 // Esta funcion es para eliminar el perfil de la empresa
-export const eliminarPerfil = async(req,res)=>{
+export const eliminarPerfil = async (req, res) => {
     try {
-        await Empresas.destroy({where:{NIT:req.userCOD}})
-        res.status(200).json({message:"Perfil eliminado correctamente"})
+        await Empresas.destroy({ where: { NIT: req.userCOD } })
+        res.status(200).json({ message: "Perfil eliminado correctamente" })
     } catch (error) {
-        res.status(400).json({message:"Hubo un error al eliminar el perfil", error:error})
+        res.status(400).json({ message: "Hubo un error al eliminar el perfil", error: error })
     }
 }
 // Esta funcion es para obtener la informacion de la empresa por el token
-export const getempresa = async(req,res)=>{
+export const getempresa = async (req, res) => {
     try {
-      const dato=req.userCOD
-      const datos=await Empresas.findOne({where:{NIT:dato}})
-      res.status(200).json(datos)
+        const dato = req.userCOD
+        const datos = await Empresas.findOne({ where: { NIT: dato } })
+        res.status(200).json(datos)
     } catch (error) {
-      if (error instanceof Sequelize.DatabaseError) {
-        // Manejar el error de base de datos
-        res.status(400).json({message: `Error de base datos`, error:error.message})
-      } else {
-          // Manejar otros tipos de errores
-          res.status(400).json({message:'Hubo un error al obtener informacion de la empresa', error});
-      }
+        if (error instanceof Sequelize.DatabaseError) {
+            // Manejar el error de base de datos
+            res.status(400).json({ message: `Error de base datos`, error: error.message })
+        } else {
+            // Manejar otros tipos de errores
+            res.status(400).json({ message: 'Hubo un error al obtener informacion de la empresa', error });
+        }
     }
 }
 
 // Esta funcion crea un nuevo servicio vinculada a la cuenta de la empresa
-export const servicio = async(req,res)=>{
+export const servicio = async (req, res) => {
     const {
         nombre,
         descripcion,
         valor_servicio,
         COD_subCategoria,
-    }= req.body
+    } = req.body
     try {
-        const servicio=await Servicios.create({
-            ID:5,
+        const servicio = await Servicios.create({
+            ID: 5,
             nombre,
             descripcion,
             valor_servicio,
@@ -102,50 +102,51 @@ export const servicio = async(req,res)=>{
             NIT_empresas: req.userCOD
         })
         let image
-        if (!req.files || req.files.length===0){
-            res.status(400).json({message:"Subir imagen"})
+        if (!req.files || req.files.length === 0) {
+            res.status(400).json({ message: "Subir imagen" })
         }
-        req.files.forEach( async(file) => {
+        req.files.forEach(async (file) => {
             image = file.filename
             await ImagesServicios.create({
-                ID_servicio:servicio.ID,
+                ID_servicio: servicio.ID,
                 image
             })
         });
-        res.status(201).json({message:"Servicio creado correctamente"})
+        res.status(201).json({ message: "Servicio creado correctamente" })
     } catch (error) {
         if (error instanceof Sequelize.DatabaseError) {
             // Manejar el error de base de datos
-            res.status(400).json({message: `Error de base datos`, error:error.message})
-          } else {
-              // Manejar otros tipos de errores
-              res.status(400).json({message:'Hubo un error al crear el servicio', error});
-          }
+            res.status(400).json({ message: `Error de base datos`, error: error.message })
+        } else {
+            // Manejar otros tipos de errores
+            res.status(400).json({ message: 'Hubo un error al crear el servicio', error });
+        }
     }
 }
 // Esta funcion sirve para editar la reserva realizada por la empresa
-export const editarServicio = async(req,res)=>{
+export const editarServicio = async (req, res) => {
     const {
         ID,
         nombre,
         descripcion,
         valor_servicio,
         COD_subCategoria,
-    }=req.body
+    } = req.body
 
     const ruta = baseDir()
     // console.log(ruta)
     try {
         const imagenes = await ImagesServicios.findAll(
-            {where:{ID_servicio:ID}
-        })
+            {
+                where: { ID_servicio: ID }
+            })
 
-        imagenes.map(async(imagen)=>{
+        imagenes.map(async (imagen) => {
             const filePath = path.join(ruta, imagen.image);
             if (fs.existsSync(filePath)) {
                 fs.unlinkSync(filePath);
                 // console.log("La ruta del archivo es: ", filePath)
-            }else{
+            } else {
                 console.log("El archivo no exite en la ubicacion")
             }
         });
@@ -156,29 +157,44 @@ export const editarServicio = async(req,res)=>{
             valor_servicio,
             COD_subCategoria,
             NIT_empresas: req.userCOD
-        },{
-            where:{ID:ID}
+        }, {
+            where: { ID: ID }
         });
 
-        await ImagesServicios.destroy({where:{ID_servicio:ID}})
+        await ImagesServicios.destroy({ where: { ID_servicio: ID } })
 
-        let image 
-        req.files.forEach( async(file) => {
+        let image
+        req.files.forEach(async (file) => {
             image = file.filename
             await ImagesServicios.create({
-                ID_servicio:ID,
+                ID_servicio: ID,
                 image
             })
         });
 
-        res.status(200).json({message:"Servicio actualizado correctamente"})
+        res.status(200).json({ message: "Servicio actualizado correctamente" })
     } catch (error) {
         if (error instanceof Sequelize.DatabaseError) {
             // Manejar el error de base de datos
-            res.status(400).json({message: `Error de base datos`, error:error.message})
-          } else {
-              // Manejar otros tipos de errores
-              res.status(400).json({message:'Hubo un error al actualizar el servicio', error});
-          }
+            res.status(400).json({ message: `Error de base datos`, error: error.message })
+        } else {
+            // Manejar otros tipos de errores
+            res.status(400).json({ message: 'Hubo un error al actualizar el servicio', error });
+        }
+    }
+}
+// Esta funcion trae a todos los servicios de la empresa
+export const getservicios = async (req, res) => {
+    try {
+        const servicios = await Servicios.findAll({ where: { NIT_empresas: req.userCOD } })
+        res.status(200).json(servicios)
+    } catch (error) {
+        if (error instanceof Sequelize.DatabaseError) {
+            // Manejar el error de base de datos
+            res.status(400).json({ message: `Error de base datos`, error: error.message })
+        } else {
+            // Manejar otros tipos de errores
+            res.status(400).json({ message: 'Hubo un error al obtener los servicios', error });
+        }
     }
 }
