f9850386ea153bfba8ff6fac05a4677a27f407ac
Juanes <juan.henao.garzon@gmail.com>
2024-05-05 02:06:21 -0500

    visualizacion de las reservas de los clientes

--- Client/src/pages/empresa/templates/reservas.jsx
+++ Client/src/pages/empresa/templates/reservas.jsx
@@ -1,39 +1,58 @@
 import { reservasDB } from "../db/reservasDB.json";
 import { NotFound } from "../../../components/templates/NotFound";
 import { ButtonAdmin } from "../../../components/buttons/buttonAdmin";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 export function Reservas() {
+  const [datos, setdatos] = useState([])
+    useEffect(()=>{
+        obtener()
+    },[])
+    const token = localStorage.getItem("token")
+    const obtener = async()=>{
+        try {
+            const respuesta= await axios.get("http://localhost:8000/empresa/reservasClientes",{
+                headers:{
+                    Authorization:token
+                }
+            })
+            setdatos(respuesta.data)
+        } catch (error) {
+            alert(error)
+        }
+    }
   return (
     <>
-      {reservasDB?.length === 0 ? (
+      {datos?.length === 0 ? (
         <NotFound />
       ) : (
-        reservasDB.map((reserva) => (
+        datos.map((reserva) => (
           <div
             className="flex  border-2 border-solid border-color_font_dark rounded-sm
             dark:bg-light_theme dark:border-light_theme z-10 "
-            key={reserva.id}
+            key={reserva.COD}
           >
             <div className="w-full flex flex-col justify-center bg-[#190042] text-white p-4
             rounded-sm dark:bg-second_color_lt z-10">
               <h3 className="font-title font-semibold text-3xl z-10" >Contrato de servicios</h3>
 
-              <p className="font-title font-semibold text-lg mt-2 z-10">Nombre de Usuario: </p>
-              <p className="font-title">{reserva.username}</p>
+              <p className="font-title font-semibold text-lg mt-2 z-10">Nombre del Usuario: </p>
+              <p className="font-title">{reserva.nombre}</p>
+
+              <p className="font-title font-semibold text-lg mt-1 z-10">Apellido: </p>
+              <p className="font-title">{reserva.apellido}</p>
 
               <p className="font-title font-semibold text-lg mt-1 z-10">Correo: </p>
               <p className="font-title">{reserva.email}</p>
 
-              <p className="font-title font-semibold text-lg mt-1 z-10">Teléfono: </p>
-              <p className="font-title">{reserva.telefono}</p>
-
-              <p className="font-title font-semibold text-lg mt-1 z-10">Dirección: </p>
-              <p className="font-title">{reserva.dirección}</p>
+              <p className="font-title font-semibold text-lg mt-1 z-10">Numero de Servicios: </p>
+              <p className="font-title">{reserva.numeroReservas}</p>
 
 
               <div className="flex  text-center flex-row-reverse z-10">
                 <ButtonAdmin
-                  href={`/empresa/reserva/reservaDescrip/${reserva.id}`}
+                  href={`/empresa/reserva/reservaDescrip/${reserva.COD}`}
                 >
                   Ver pedidos
                 </ButtonAdmin>
