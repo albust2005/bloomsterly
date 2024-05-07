674c845fb3add6b463405d1d94571c65e0b7c47b
silvana2828 <silvanaxuluaga0@gmail.com>
2024-05-07 15:16:54 -0500

    Reserva

--- Client/src/pages/empresa/reservaDescrip.jsx
+++ Client/src/pages/empresa/reservaDescrip.jsx
@@ -8,31 +8,37 @@ export function ReservaDescrip() {
   const { id } = useParams();
 
   const reservas = useReservasContext();
-  const reserva = reservas.find((reserva) => reserva.id === id);
+  const reserva = reservas?.find((reserva) => reserva.id === id);
 
   return (
     <>
       <TitleAE title="Reservación" descripcion="" />
       <div className="p-[1.5vh] flex gap-4 z-10">
-        <div className="bg-[#190042]  dark:bg-light_theme p-[1.5vh] 
-        rounded-sm h-full text-white dark:text-second_color_lt w-full ">
-          <p className=" font-title font-semibold text-2xl">N° de contrato: {reserva.id}</p>
+        <div
+          className="bg-[#190042]  dark:bg-light_theme p-[1.5vh] 
+        rounded-sm h-full text-white dark:text-second_color_lt w-full "
+        >
+          <p className=" font-title font-semibold text-2xl">
+            N° de contrato: {reserva?.id}
+          </p>
 
-          <p className="font-title font-semibold text-lg mt-2">Nombre de Usuario: </p>
-          <p className="font-title">{reserva.username}</p>
+          <p className="font-title font-semibold text-lg mt-2">
+            Nombre de Usuario:{" "}
+          </p>
+          <p className="font-title">{reserva?.username}</p>
 
           <p className="font-title font-semibold text-lg mt-2">Correo: </p>
-          <p className="font-title">{reserva.email}</p>
+          <p className="font-title">{reserva?.email}</p>
 
           <p className="font-title font-semibold text-lg mt-2">Teléfono: </p>
-          <p className="font-title">{reserva.telefono}</p>
+          <p className="font-title">{reserva?.telefono}</p>
 
           <p className="font-title font-semibold text-lg mt-2">Dirección: </p>
-          <p className="font-title">{reserva.direccion}</p>
+          <p className="font-title">{reserva?.direccion}</p>
         </div>
-          <div>
-            <ServiciosReserva servicios={reserva.servicios} />
-          </div>
+        <div>
+          <ServiciosReserva servicios={reserva?.servicios} />
+        </div>
       </div>
     </>
   );
