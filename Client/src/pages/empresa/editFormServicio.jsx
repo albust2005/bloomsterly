1506e9cdf5d15227381650ea17e43647dbcec0c0
Jkose7 <ojosema10@gmail.com>
2024-05-09 00:01:24 -0500

    correcion editar servicio - empresa

--- Client/src/pages/empresa/editFormServicio.jsx
+++ Client/src/pages/empresa/editFormServicio.jsx
@@ -81,7 +81,7 @@ export const EditarFormServicio = () => {
         },
       });
       showToastMessage(respuesta.data.message);
-      navegate(`servicios`);
+      navegate(`/empresa/servicios`);
     } catch (error) {
       showToastMessage("Hubo un error al editar el servicio");
     }
