f6d4e6e5076a2b08d4e1f1a72c39ede141579cb2
Jkose7 <ojosema10@gmail.com>
2024-05-09 00:20:55 -0500

    correcion header

--- Client/src/components/buttons/buttonNav.jsx
+++ Client/src/components/buttons/buttonNav.jsx
@@ -4,8 +4,8 @@ export function ButtonNav ({text, href}){
     return(
         <Link 
         to={href} 
-        className="font-title italic text-white text-lg bg-transparent 
-        hover:bg-purple-900 px-5 py-5 md:py-1 rounded-lg hover:ease-in-out duration-300 z-10
+        className="font-title italic text-white text-md md:text-lg bg-transparent 
+        hover:bg-purple-900 px-5 py-2 md:py-1 rounded-lg hover:ease-in-out duration-300 z-10
         dark:text-second_color_lt hover:dark:bg-[#ebd1b8]">
         {text}
         </Link>
