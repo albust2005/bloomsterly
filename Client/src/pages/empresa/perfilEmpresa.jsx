d981d9d9655ea340dc00e899e97b885b935ede81
Jkose7 <ojosema10@gmail.com>
2024-05-09 01:37:06 -0500

    agregando Cliente en perfil

--- Client/src/pages/empresa/perfilEmpresa.jsx
+++ Client/src/pages/empresa/perfilEmpresa.jsx
@@ -1,49 +1,57 @@
 import { ButtonAdmin } from "../../components/buttons/buttonAdmin";
 import { IconUser } from "../administrador/templates/iconUser";
-// import { ServiciosReserva } from "./templates/serviciosReserva";
 import { useUserContext } from "../../components/providers/userProvider";
 import { EliminarAdmin } from "../administrador/eliminarAdmin";
 import { useEffect, useState } from "react";
 import axios from "axios";
-export function PerfilEmpresa() {
-  const [cambiar, setCambiar] = useState(false);
-  const [dato, setdato] = useState([])
-  const [dato1, setdato1] = useState([])
-  const eliminar = () => {
-    setCambiar(!cambiar); 
-  };
-  const { sesionUser } = useUserContext();
 
-  const obtenerUser=async()=>{
+export const useFoundUser = () => {
+  const token = localStorage.getItem("token")
+  const { sesionUser } = useUserContext();
+  const [dataUser, setDataUser] = useState({})
+
+  const obtenerUser = async () => {
     try {
-      const respuesta = await axios.get("http://localhost:8000/user/getusuario",{
-        headers:{
-          Authorization:token
+      const respuesta = await axios.get("http://localhost:8000/user/getusuario", {
+        headers: {
+          Authorization: token
         }
       })
-      setdato(respuesta.data)
+      setDataUser(respuesta.data)
     } catch (error) {
       alert(error)
       console.log(error)
     }
   }
-  
-  const obtenerEmpresa=async()=>{
+
+  const obtenerEmpresa = async () => {
     try {
-      const respuesta = await axios.get("http://localhost:8000/empresa/getempresa",{
-        headers:{
-          Authorization:token
+      const respuesta = await axios.get("http://localhost:8000/empresa/getempresa", {
+        headers: {
+          Authorization: token
         }
       })
-      setdato1(respuesta.data)
+      setDataUser(respuesta.data)
     } catch (error) {
       alert(error)
     }
   }
 
-  const token=localStorage.getItem("token")
-  useEffect(()=>{
-    if (sesionUser?.Rol === "Administrador"){
+  const obtenerAdmin = async () => {
+    try {
+      const respuesta = await axios.get("http://localhost:8000/admin/getadmin", {
+        headers: {
+          Authorization: token
+        }
+      })
+      setDataUser(respuesta.data)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  useEffect(() => {
+    if (sesionUser?.Rol === "Administrador") {
       obtenerAdmin()
     }
     if (sesionUser?.Rol === "Empresa") {
@@ -52,122 +60,113 @@ export function PerfilEmpresa() {
     if (sesionUser?.Rol === "Cliente") {
       obtenerUser()
     }
-  },[])
+  }, [])
 
-  const obtenerAdmin=async()=>{
-    try {
-      const respuesta = await axios.get("http://localhost:8000/admin/getadmin",{
-        headers:{
-          Authorization:token
-        }
-      })
-      setdato(respuesta.data)
-    } catch (error) {
-      alert(error)
-    }
-  }
+  console.log(dataUser)
+  return ({ dataUser, sesionUser })
+}
 
+
+
+export function PerfilEmpresa() {
+  const [cambiar, setCambiar] = useState(false);
+  const eliminar = () => {
+    setCambiar(!cambiar);
+  };
+
+  const { dataUser, sesionUser } = useFoundUser()
 
   return (
     <>
-      <section className="flex  w-full justify-center gap-4 z-10">
-        <div className="flex w-full">
+      <section className="flex w-full min-h-64 justify-center gap-4 z-10">
+        <div className="flex w-full min-h-full">
           <div
-            className="w-full flex flex-col justify-center items-center font-title font-bold 
+            className="w-1/2 flex flex-col justify-center items-center font-title font-bold 
           bg-hover_boton_admin dark:bg-light_theme"
           >
-            {dato1.image ? <img src={dato1.image ? `http://localhost:8000/empresa/getempresa/${dato1.image}` : ""} alt="imagen" width={100} height={100} style={{borderRadius:12}}></img>  : <IconUser clasName="h-20" />}
-            {sesionUser?.Rol === "Administrador" ? (
-              <h2 className="text-white dark:text-second_color_lt celular:text-[20px] sm:text-[30px] lg:text-4xl">
-                {dato.nombre}
-              </h2>
-            ) : (
-              <>
-              <h2 className="text-white text-4xl dark:text-second_color_lt celular:text-[20px] sm:text-[30px] lg:text-4xl">
-                {dato1.nombre}
-              </h2>
-              </>
-            )}
+            {dataUser.image ? <img src={dataUser.image ? `http://localhost:8000/empresa/getempresa/${dataUser.image}` : ""} alt="imagen" width={100} height={100} style={{ borderRadius: 12 }}></img> : <IconUser clasName="h-20" />}
+
+            <h2 className="text-white text-4xl dark:text-second_color_lt celular:text-[20px] sm:text-[30px] lg:text-4xl">
+              {dataUser.username}
+            </h2>
+
           </div>
-          <div className="w-[100%] bg-[#190042] rounded-sm p-5 font-title text-white dark:bg-second_color_lt">
-            {sesionUser?.Rol === "Administrador" ? (
-              <>
-                <p className="font-semibold md:text-xl celular:text-[15px] lg:text-2xl">
-                  Cédula
-                </p>
-                <p className="celular:text-[15px] lg:text-[lg]">{dato.COD}</p>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Nombre
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">{dato.nombre}</p>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Primer Apellido
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato.primer_apelli}
-                </p>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Nombre De Usuario
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato.username}
-                </p>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Gmail
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">{dato.email}</p>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Municipio
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato.municipio?.municipio}
-                </p>
-              </>
-            ) : (
-              <>
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Descripción
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.descripcion}
-                </p>
 
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Dirección
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.direccion}
-                </p>
+          <div className="w-1/2 bg-[#190042] rounded-sm p-5 font-title text-white dark:bg-second_color_lt">
 
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Teléfono
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.telefono}
-                </p>
+            <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+              Nombre
+            </p>
+            <p className="lg:text-lg celular:text-[15px]">
+              {sesionUser.Rol === 'Cliente' ? dataUser.nombre_c : dataUser.nombre}
+            </p>
 
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Correo electrónico
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.email}
-                </p>
+            <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+              Email
+            </p>
+            <p className="lg:text-lg celular:text-[15px]">
+              {dataUser.email}
+            </p>
 
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Instagram
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.instagram}
-                </p>
+            <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+              Username
+            </p>
+            <p className="lg:text-lg celular:text-[15px]">
+              {dataUser.username}
+            </p>
 
-                <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
-                  Facebook
-                </p>
-                <p className="lg:text-lg celular:text-[15px]">
-                  {dato1.facebook}
-                </p>
-              </>
-            )}
+
+            {
+              sesionUser.Rol !== 'Empresa'
+                ?
+                <>
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Apellido
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.primer_apelli}
+                  </p>
+                </>
+                :
+                <>
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Descripción
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.descripcion}
+                  </p>
+
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Dirección
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.direccion}
+                  </p>
+
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Teléfono
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.telefono}
+                  </p>
+
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Instagram
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.instagram}
+                  </p>
+
+                  <p className="font-semibold text-2xl celular:text-[15px] md:text-xl lg:text-2xl">
+                    Facebook
+                  </p>
+                  <p className="lg:text-lg celular:text-[15px]">
+                    {dataUser.facebook}
+                  </p>
+                </>
+            }
+
+
 
             <div className="flex flex-row-reverse gap-4 mt-2">
               {sesionUser?.Rol === "Administrador" ? (
